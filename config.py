b73986381955067d294baa83a7463c634cbfdaf6
Julian Schrittwieser <j.schrittwieser@gmail.com>
2013-10-27 16:27:08 +0000

    keep old domain name

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ RECIPIENT_NAME = "Julian Schrittwies"
 RECIPIENT_EMAIL = "j.schrittwieser@gmail.com"
 
 DIARY_NAME = "Infinite Diary"
-DIARY_EMAIL ="diary@furida.mu"
+DIARY_EMAIL ="diary@furidamu.org"
