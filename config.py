5c9f8f57c7c5504c48bdddafd08be3d6c44e25df
Julian Schrittwieser <j.schrittwieser@gmail.com>
2022-02-13 18:10:30 +0000

    send email directly from appengine address

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ RECIPIENT_NAME = "Julian Schrittwies"
 RECIPIENT_EMAIL = "j.schrittwieser@gmail.com"
 
 DIARY_NAME = "Infinite Diary"
-DIARY_EMAIL = "diary@furidamu.org"
+DIARY_EMAIL = "diary@infinite-diary.appspotmail.com"
 TODO_EMAIL = "todo@furidamu.org"
 
 BACKUP_KEY = 'Cev2pDLLAOiO5YMsrgmWaMytysXDmy'
