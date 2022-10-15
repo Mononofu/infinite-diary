44aa87fc335ec4da43b5a3c6999d7af18d8bf268
Julian Schrittwieser <j.schrittwieser@gmail.com>
2022-10-15 18:26:36 +0100

    don't track config

--- config.py
+++
@@ -1,8 +0,0 @@
-RECIPIENT_NAME = "Julian Schrittwies"
-RECIPIENT_EMAIL = "j.schrittwieser@gmail.com"
-
-DIARY_NAME = "Infinite Diary"
-DIARY_EMAIL = "diary@infinite-diary.appspotmail.com"
-TODO_EMAIL = "todo@furidamu.org"
-
-BACKUP_KEY = 'Cev2pDLLAOiO5YMsrgmWaMytysXDmy'
